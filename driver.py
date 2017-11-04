21bd29a7c105d8e613f7cea0ecc8606f468be637
stulich <stephenulich@gmail.com>
2017-11-04 16:44:52 -0400

    changes to continuously monitor for commands

--- driver.py
+++ driver.py
@@ -1,17 +1,8 @@
 from socket import *
-import directions
+#import directions
 
 # Set server port for the socket
-serverPort = 25565
-
-# Create a server socket
-serverSocket = socket(AF_INET, SOCK_STREAM)
-
-# "bind" the socket to the localhost and server port.
-serverSocket.bind(("", serverPort))
-
-# listen(backlog), where backlog is the maximum number of queued connections
-serverSocket.listen(5)
+serverPort = 25570
 
 # dictionary of commands that map to functions
 commandToFunction = {
@@ -27,25 +18,43 @@ commandToFunction = {
     'go left': 'directions.turnLeft()',
 }
 
-try:
-    # Accepts a new connection. addr is the address of the incoming connection
-    Socket_connection, addr = serverSocket.accept()
+# Create a server socket
+serverSocket = socket(AF_INET, SOCK_STREAM)
 
+# "bind" the socket to the localhost and server port.
+serverSocket.bind(("", serverPort))
+
+# listen(backlog), where backlog is the maximum number of queued connections
+serverSocket.listen(5)
+
+
+try:
+    print('waiting for new connection')
+
+    #runs loop to constantly monitor for input
     while 1:
-        # Data received over the buffer which is string containing command
+        # Accepts a new connection. addr is the address of the incoming connection
+        Socket_connection, addr = serverSocket.accept()
+        print('connected')
+
+        # direction received over the buffer which is string containing command
         direction = Socket_connection.recv(1024)
+        direction=direction.decode('utf-8')
+
+        #splices direction to remove encoding
+        direction = direction[2:].lstrip(' ')
+        print(direction)
 
         # if the command is recognized execute the command
         if commandToFunction.hasKey(direction):
-            exec(commandToFunction(direction))
-
-        # else pass
+            exec(commandToFunction[direction])
         else:
              pass
 
 #if error occurs
-except:
-    print('An error has occured')
+except KeyboardInterrupt as c:
+    print(c)
+    serverSocket.close()
 
 #closes socket at end
 finally:
