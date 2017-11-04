6893db7893a0f3e11fd0deac287248c9cb706d36
Arthur Normand <arthur@normand.space>
2017-11-04 22:21:22 +0000

    Added a file to allow the robot to be controlled by a ckeyboard

---
+++ keyboardDriver.py
@@ -0,0 +1,34 @@
+import curses
+import directions as robot
+
+def driver(win):
+    win.nodelay(True)
+    key=""
+    win.clear()
+    win.addstr("Detected Key:")
+    win.nodelay(False)
+    while 1:
+        try:
+            #key=win.getkey()
+            key=win.getch()
+            win.clear()
+            win.addstr("Detected Key:")
+            win.addstr(str(key))
+            
+            if key == 10:
+                break
+            if key == 32:
+                robot.stop()
+            if key == 258:
+                robot.goBackward()
+            if key == 259:
+                robot.goForward()
+            if key == 260:
+                robot.turnLeft()
+            if key == 261:
+                robot.turnRight()
+        except Exception as e:
+            pass
+
+curses.wrapper(driver)
+
