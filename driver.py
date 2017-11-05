b4283c40151f836d8c25101c41748c100a0985cb
Conor Carmichael <ccarmichael@umass.edu>
2017-11-05 11:38:27 -0500

    Removed light thread

--- driver.py
+++ driver.py
@@ -36,8 +36,8 @@ serverSocket.listen(5)
 try:
     print('waiting for new connection')
     # creates new thread to monitor distance in front of the machine
-    t = threading.Thread(target = directions.start_led_controller)
-    t.start()
+    # t = threading.Thread(target = directions.start_led_controller)
+    # t.start()
 
     #runs loop to constantly monitor for input
     while 1:
